cf1ef47747a553792ff9cf9b2fe14a826b615739
Anton Ivanov <ivanova21600@mail.ru>
2025-11-02 22:49:12 +0300

    refactoring

--- src/main/java/ru/ivanov/ecommerceplatformproject/pageservice/controller/PageController.java
+++ src/main/java/ru/ivanov/ecommerceplatformproject/pageservice/controller/PageController.java
@@ -10,7 +10,7 @@ public class PageController {
 
     @GetMapping("/shop")
     public String shopPage() {
-        return "forward:/index_multi_with_kabinet_updated.html";
+        return "forward:/index_multi2.html";
     }
 
     @GetMapping("/seller")
